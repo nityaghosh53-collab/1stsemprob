d951d1f262286beb65affde026b159d7f0b96ef9
Nitya Ghosh <nityaghosh53@gmail.com>
2026-09-23 22:45:52 +0530

    positive

---
+++ classifynum.js
@@ -0,0 +1,11 @@
+function classifyNumber(number) {
+    if (number > 0) {
+        return "Positive";
+    } else if (number < 0) {
+        return "Negative";
+    } else {
+        return "Zero";
+    }
+}
+
+console.log(classifyNumber(18));
